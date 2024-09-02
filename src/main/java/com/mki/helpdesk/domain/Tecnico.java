7596a79c8daf68240a56bf0d3584a992ab08d851
mkiaraujo <mki.araujo@gmail.com>
2024-09-02 19:22:02 -0300

    anotacoes JPA para classes de dominio

--- src/main/java/com/mki/helpdesk/domain/Tecnico.java
+++ src/main/java/com/mki/helpdesk/domain/Tecnico.java
@@ -1,17 +1,26 @@
 package com.mki.helpdesk.domain;
 
+import com.mki.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico extends Pessoa{
+@Entity
+public class Tecnico extends Pessoa implements Serializable {
 
+    @OneToMany(mappedBy = "tecnico" )
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
