b030fe5a88306cac8a01e018030715089520a494
mkiaraujo <mki.araujo@gmail.com>
2024-09-02 14:04:55 -0300

    tipos enumerados criados

---
+++ src/main/java/com/mki/helpdesk/domain/enums/Status.java
@@ -0,0 +1,37 @@
+package com.mki.helpdesk.domain.enums;
+
+public enum Status {
+
+    ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(2, "ENCERRADO");
+
+    private Integer codigo;
+    private String descricao;
+
+    Status(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static Status toEnum(Integer cod) {
+        if(cod == null) {
+            return null;
+        }
+
+        for (Status status : Status.values()) {
+            if(cod.equals(status.getCodigo())) {
+                return status;
+            }
+        }
+
+        throw new IllegalArgumentException("Status inválido");
+
+    }
+}
