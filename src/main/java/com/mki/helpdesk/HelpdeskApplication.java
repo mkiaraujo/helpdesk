609e07fea6c159cd8d4fa5ada26d2e536e3b6058
mkiaraujo <mki.araujo@gmail.com>
2024-09-02 22:22:01 -0300

    carga inicial da base de dados

--- src/main/java/com/mki/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/mki/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,48 @@
 package com.mki.helpdesk;
 
+import com.mki.helpdesk.domain.Chamado;
+import com.mki.helpdesk.domain.Cliente;
+import com.mki.helpdesk.domain.Tecnico;
+import com.mki.helpdesk.domain.enums.Perfil;
+import com.mki.helpdesk.domain.enums.Prioridade;
+import com.mki.helpdesk.domain.enums.Status;
+import com.mki.helpdesk.repositories.ChamadoRepository;
+import com.mki.helpdesk.repositories.ClienteRepository;
+import com.mki.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class HelpdeskApplication {
+public class HelpdeskApplication implements CommandLineRunner {
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Tecnico tec1 = new Tecnico(null, "Valdir cezar", "36087289534", "valdir@mail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "32242702092", "torvalds@mail.com", "123");
+
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "chamado 01", "Primeiro chamado", tec1, cli1);
+
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+	}
 }
