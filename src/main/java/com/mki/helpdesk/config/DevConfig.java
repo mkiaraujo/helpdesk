efceb902a742ae51971d2ec32b55126ddc7f4835
mkiaraujo <mki.araujo@gmail.com>
2024-09-02 23:47:36 -0300

    perfil de desenvolvimento

--- src/main/java/com/mki/helpdesk/config/DevConfig.java
+++ src/main/java/com/mki/helpdesk/config/DevConfig.java
@@ -2,19 +2,26 @@ package com.mki.helpdesk.config;
 
 import com.mki.helpdesk.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
     @Autowired
     private DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
     @Bean
     public Boolean instanciaDB() {
-        this.dbService.instanciaDB();
-        return true;
+        if (value.equals("create")) {
+            this.dbService.instanciaDB();
+            return true;
+        }
+        return false;
     }
 }
