f29116fc3258569f4f0a681ccecf8d8fe119bd2d
mkiaraujo <mki.araujo@gmail.com>
2024-09-04 11:16:15 -0300

    endpoint findById para Tecnico

---
+++ src/main/java/com/mki/helpdesk/services/TecnicoService.java
@@ -0,0 +1,20 @@
+package com.mki.helpdesk.services;
+
+import com.mki.helpdesk.domain.Tecnico;
+import com.mki.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    public Tecnico findById(Integer id) {
+        Optional<Tecnico> obj = tecnicoRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
