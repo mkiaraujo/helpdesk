a5d2087e75449941d208bbf1e86ec624556628d0
mkiaraujo <mki.araujo@gmail.com>
2024-09-16 23:24:03 -0300

    findAll para chamado

--- src/main/java/com/mki/helpdesk/services/ChamadoService.java
+++ src/main/java/com/mki/helpdesk/services/ChamadoService.java
@@ -1,12 +1,15 @@
 package com.mki.helpdesk.services;
 
 import com.mki.helpdesk.domain.Chamado;
+import com.mki.helpdesk.domain.dtos.ChamadoDTO;
 import com.mki.helpdesk.repositories.ChamadoRepository;
 import com.mki.helpdesk.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ChamadoService {
@@ -18,5 +21,12 @@ public class ChamadoService {
         Optional<Chamado> obj = chamadoRepository.findById(id);
         return obj.orElseThrow( () -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
 
+    }
+
+    public List<ChamadoDTO> findAll() {
+        return chamadoRepository.findAll()
+                .stream()
+                .map(chamado -> new ChamadoDTO(chamado))
+                .collect(Collectors.toList());
     }
 }
